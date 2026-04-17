ecf196edb4223457eef43353c07050996fe49cb9
fiorella195 <fiorella15082007@gmail.com>
2026-04-17 11:03:08 -0600

    trabajo api

--- app.js
+++ app.js
@@ -1,69 +1,102 @@
-    const express = require (`express`)
-    const app = express()
-    const port = 3000;
+const express = require(`express`);
+const app = express();
+const port = 3000;
 
-    app.use(express.json());
+app.use(express.json());
 
-    let books = [
-        {
-            id:1,
-            title: `cien años de soledad`,
-            author: `Gabriel Garcia Marquez`
-        },
-        {
-            id:2,
-            title: `Don Quijote`,
-            author: `Migel de cervates`
-        }
-    ];
-//obtener todos los libros 
-    app.get (`/api/books`, (req, res) => {
-        res.json(books);
+let books = [
+  {
+    id: 1,
+    title: "Cien años de soledad",
+    author: "Grabriel Garcia Márquez",
+  },
+  {
+    id: 2,
+    title: "Don Quijote",
+    author: "Miguel de Cervantes",
+  },
+];
 
-    });
-//obtener un libro por ID
-app.get('/api/books:id',(req, res) => {
-
-    const id = parseInt( req.params.id);
-
-    const  books = books.find (b => b. id === id);
-
-    if (book) {
-        return
-        res.status(404).json({message: 'Libro no encontrado'
-
-        })
-    }
-})
-
-//crear un nuevo libro
-app.post('/api/books',(req,res) => {
-
-    const {title, author} =  req.body;
-
-    if (!title || !author) {
-        return res.status(400).json({
-            message: 'Dato obligatorio'
-        });
-    }
-
-    const newBooks = {
-        id: books.length > 0 ? books[books.length - 1].id + 1 : 1,
-        title,
-        author
-    };
-
-    books.push(newBooks);
-
-    res.status(201).json(newBooks);
-
+//todos los libros
+app.get("/api/books", (req, res) => {
+  res.json(books);
 });
 
-    app.get(`/`,(req,res)=> {
-        res.send(`hello todos
-            `);
-    })
+//todos los libros por ID
+app.get("/api/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
 
-    app.listen(port,() => {
-        console.log(`Servidor corriendo en http://localhost:${port}`);
+  const book = books.find((b) => b.id === id);
+
+  if (!book) {
+    return res.status(404).json({
+      message: "Libro no encontrado",
     });
+  }
+
+  res.json(book);
+});
+
+//crear un nuevo libro
+app.post("/api/books", (req, res) => {
+  const { title, author } = req.body;
+
+  console.log(title);
+  /*
+  if (!title || !author) {
+    return res.status(400).json({
+      message: "Datos obliigatorios",
+    });
+  }
+
+  const newBook = {
+    id: books.length > 0 ? books[books.length - 1].id + 1 : 1,
+    title,
+    author,
+  };
+
+  books.push(newBook);
+
+  res.status(201).json(newBook);
+*/
+});
+/*
+//Actualizar un libro existente
+app.put("/api/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, author } = req.body;
+
+  const book = books.find((b) => b.id === id);
+
+  if (!book) {
+    return res.status(404).json({
+      message: "No encontrado",
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello pitti!");
+});
+
+app.delete("/api/books/:id", (red, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = books.findIndex((b) => b.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: "NO Encontrado",
+    });
+  }
+
+  books.splice(index, 1);
+
+  res.json({
+    message: "Eliminado",
+  });
+});
+*/
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
